40016a17700d0178e57c1b6a59a61834e4b1dffc
eyal molad <eyal.molad@gmail.com>
2021-02-28 19:55:15 +0100

    build

--- publish.bat
+++ publish.bat
@@ -1,9 +1,11 @@
 @echo off
+call pull.bat
+choice /d y /t 3 > nul
 call hugo
 choice /d y /t 3 > nul
 git config core.autocrlf true
 git add .
 git commit -m "build"
-git push https://%PERSONAL_GIT%@github.com/eyalmolad/gotask.net.git
+git push https://%PERSONAL_GIT%@github.com/gotask.net.git
 
 
---
+++ pull.bat
@@ -0,0 +1,3 @@
+@echo off
+git config core.autocrlf true
+git pull https://github.com/eyalmolad/gotask.net.git
